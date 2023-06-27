b1d29fd92149a7d1f610d87337b10625b5a377ca
Hiro7110 <130155464+Hiro7110@users.noreply.github.com>
2023-06-27 22:09:37 +0900

    Create adding_tax_by_using_def.py

---
+++ kadai_013/adding_tax_by_using_def.py
@@ -0,0 +1,15 @@
+# 課題の提出
+def add_tax (price, tax):
+  total = price + tax
+
+  print(f"The total cost is {total} yen.")
+
+add_tax(500, 50)
+
+# 課題とは関係ないですが、別の方法でも計算してみました。
+# ただtotalのtypeがfloatになってしまったのが気になりました。
+def add_tax (price):
+  tax = price / 10
+  total = price + tax
+
+  print(f"The total cost is {total} yen.")
